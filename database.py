817a545e7cea758a7b483c70a1371e937cfab02d
ITRSFCompany1 <ITRSF_Industries@outlook.com>
2026-05-04 17:00:57 -0600

    Refactor database connection string initialization

--- database.py
+++ database.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-db_path = os.path.join(BASE_DIR, "itrsf.db")
+SQLALCHEMY_DATABASE_URL = "sqlite:///./itrsf.db"
 
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{db_path}"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
